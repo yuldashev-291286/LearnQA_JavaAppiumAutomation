b0184f9af988008aa6a58969e713fb8db5ffa5b2
yuldashev-291286 <56519643+yuldashev-291286@users.noreply.github.com>
2025-05-08 20:18:40 +0300

    Занятие 10. Allure 2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
+
 }
+
 
 android {
     namespace = "com.example.javaappiumautomation"
@@ -14,6 +16,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     }
 
     buildTypes {
@@ -29,6 +32,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    defaultConfig {
+        testInstrumentationRunner = "io.qameta.allure.espresso.AllureAndroidRunner"
+    }
+
 }
 
 dependencies {
@@ -41,8 +49,30 @@ dependencies {
     implementation(files("src\\main\\java\\com\\example\\javaappiumautomation\\libs\\junit-4.12.jar"))
     implementation(files("src\\main\\java\\com\\example\\javaappiumautomation\\libs\\java-client-4.1.2.jar"))
     implementation(libs.play.services.fido)
+    implementation(libs.support.annotations)
+    implementation(libs.annotation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.monitor)
+
+    androidTestImplementation (libs.allure.android.commons)
+    androidTestImplementation (libs.allure.android.model)
+    androidTestImplementation (libs.allure.espresso)
+
+    androidTestImplementation (libs.kotlin.stdlib)
+    androidTestImplementation (libs.junit.v412)
+    androidTestImplementation (libs.uiautomator)
+
 }
+
+repositories {
+//    mavenCentral()
+//    maven {
+//        url; //"https://dl.bintray.com/qameta/maven"
+//    }
+
+}
+
+
+
